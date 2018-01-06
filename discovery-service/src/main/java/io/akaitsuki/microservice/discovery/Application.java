6678c73dc9e037977ac678e82519b1594ddc4d33
AkaiTsuki <liu.jiach@husky.neu.edu>
2018-01-06 15:02:50 -0500

    Add default discovery service example

---
+++ discovery-service/src/main/java/io/akaitsuki/microservice/discovery/Application.java
@@ -0,0 +1,16 @@
+package io.akaitsuki.microservice.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by jiachiliu on 1/6/18.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
